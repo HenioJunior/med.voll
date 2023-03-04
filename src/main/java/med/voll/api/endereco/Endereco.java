bb0288b71d9056b3bbc1486598fd861c5d815b3a
Henio Junior <henioajunior@gmail.com>
2023-03-04 12:05:55 -0300

    Criação das Entidades JPA

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,55 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String complemento;
+    private String numero;
+
+    public Endereco() {
+    }
+
+    public Endereco(String logradouro, String bairro, String cep, String cidade, String uf, String complemento, String numero) {
+        this.logradouro = logradouro;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.uf = uf;
+        this.complemento = complemento;
+        this.numero = numero;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+}
---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,69 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import med.voll.api.endereco.Endereco;
+
+import java.util.Objects;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+public class Medico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Medico() {}
+
+    public Medico(String nome, String email, String crm, Especialidade especialidade, Endereco endereco) {
+        this.nome = nome;
+        this.email = email;
+        this.crm = crm;
+        this.especialidade = especialidade;
+        this.endereco = endereco;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public Especialidade getEspecialidade() {
+        return especialidade;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Medico medico)) return false;
+        return Objects.equals(getNome(), medico.getNome()) && Objects.equals(getEmail(), medico.getEmail()) && Objects.equals(getCrm(), medico.getCrm()) && getEspecialidade() == medico.getEspecialidade();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome(), getEmail(), getCrm(), getEspecialidade());
+    }
+}
